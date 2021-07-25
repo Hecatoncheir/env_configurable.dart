864d908c39afbc9d918aabdfb75a1c1dcc35f574
Vitaliy Vostrikov <vostrikov.vitaliy.mikhailovich@gmail.com>
2021-07-25 03:35:34 +0300

    Adding tests.

--- env_configurable/test/env_configurable_generate_from_environment_function_test.dart
+++ env_configurable/test/env_configurable_generate_from_environment_function_test.dart
@@ -1,1 +1,63 @@
-void main() {}
+import 'package:env_configurable/env_configurable_generator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('EnvConfigurableGenerator', () {
+    test('can check support type', () {});
+    test('can parse field value', () {});
+    test('can generate "FromEnvironmentFunction"', () {
+      final classStructure = {
+        "Class": "class Person",
+        "ClassName": "Person",
+        "ClassFields": [
+          {
+            "Field": "String firstName",
+            "FieldType": "String",
+            "FieldName": "firstName",
+            "defaultValue": "Test name"
+          },
+          {
+            "Field": "String?",
+            "FieldType": "String?",
+            "FieldName": "secondName",
+            "environmentKey": "SecondName"
+          },
+          {
+            "Field": "int age",
+            "FieldType": "int",
+            "FieldName": "age",
+          },
+          {
+            "Field": "bool isOk",
+            "FieldType": "bool",
+            "FieldName": "isOk",
+          },
+        ]
+      };
+
+      final generator = EnvConfigurableGenerator();
+
+      final createdFunction =
+          generator.generateFromEnvironmentFunction(classStructure);
+
+      expect(createdFunction, isNotEmpty);
+
+      final expectedCreatedFunction = '''
+Person _\$PersonFromEnvironment(){
+final personInstance = Person(
+firstName: "Test name",
+secondName: null,
+age: 0,
+isOk: false,
+);
+
+return personInstance;
+}
+''';
+
+      expect(expectedCreatedFunction, equals(expectedCreatedFunction));
+    });
+
+    test('can prepare class structure', () {});
+  });
+}
