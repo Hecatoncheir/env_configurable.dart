8a177ff974912fdfcc66921b2c927b5ac40e4301
Vitaliy Vostrikov <vostrikov.vitaliy.mikhailovich@gmail.com>
2023-05-17 10:46:04 +0300

    Update parseFieldValue function syntax.

--- env_configurable/lib/parse_field_value.dart
+++ env_configurable/lib/parse_field_value.dart
@@ -4,48 +4,19 @@
 /// int - 0 by default,
 /// double - 0.0 by defaultValue,
 /// String - '' empty by default.
-Object? parseFieldValue(String type, String? value) {
-  Object? _value = null;
-
-  if (type == 'String?') {
-    _value = value == null ? null : value;
-  }
-
-  if (type == 'String') {
-    _value = value == null ? '' : value;
-  }
-
-  if (type == 'bool?') {
-    _value = value == null
-        ? null
-        : value == 'true'
-            ? true
-            : false;
-  }
-
-  if (type == 'bool') {
-    _value = value == null
+Object? parseFieldValue(String typeName, String? value) {
+  return switch (typeName) {
+    'String?' => value == null ? null : value,
+    'String' => value == null ? '' : value,
+    'bool?' =>
+      value == null ? null : bool.tryParse(value, caseSensitive: false),
+    'bool' => value == null
         ? false
-        : value == 'true'
-            ? true
-            : false;
-  }
-
-  if (type == 'int?') {
-    _value = value == null ? null : int.tryParse(value) ?? 0;
-  }
-
-  if (type == 'int') {
-    _value = value == null ? 0 : int.tryParse(value) ?? 0;
-  }
-
-  if (type == 'double?') {
-    _value = value == null ? null : double.tryParse(value) ?? 0.0;
-  }
-
-  if (type == 'double') {
-    _value = value == null ? 0.0 : double.tryParse(value) ?? 0.0;
-  }
-
-  return _value;
+        : bool.tryParse(value, caseSensitive: false) ?? false,
+    'int?' => value == null ? null : int.tryParse(value),
+    'int' => value == null ? 0 : int.tryParse(value) ?? 0,
+    'double?' => value == null ? null : double.tryParse(value),
+    'double' => value == null ? 0.0 : double.tryParse(value) ?? 0.0,
+    _ => null,
+  };
 }
